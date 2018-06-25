518c8ffe8170c30dde7e7244b7a6c087decd9d43
kevin <kevinelsen@hotmail.fr>
2018-06-25 15:36:08 +0200

    exo1

---
+++ script.js
@@ -0,0 +1,8 @@
+//Déclaration de la fonction.
+function img(){
+//Déclaration d'une variable.
+var image = document.getElementById('image1');
+//Changement de la bordure en rouge avec une largeur de 3px
+  image.style.borderColor = 'red';
+  image.border = '3px';
+}
